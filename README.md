77a6aca0b67ef14bd3b530bb1d0c2e95fafefd82
Shoom UW <shoom2uniwri@gmail.com>
2022-01-23 00:30:36 +0900

    Update README.md

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # sc_server
 ソケットプログラミング、やりてえよな。多分無理。
 
-# 参考資料
+# References
 https://itsakura.com/csharp-socket<br>
 https://www.javadrive.jp/regex-basic/sample/index4.html
---
+++ sc_server/Server.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+
+namespace sc_server
+{
+    class Server
+    {
+        static string input;
+        // 
+        static IPAddress host;
+
+        static void Main()
+        {
+            Connect();
+
+
+        }
+
+        static void Connect()
+        {
+            // サーバのIPアドレスを入力
+            Console.Write("IP address: ");
+            input = Console.ReadLine();
+
+            /*
+             * 不正な入力でないか正規表現を使って判断する 
+             * 不正であればもう一度入力
+             */
+            if (Regex.IsMatch(input, @"^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"))
+            {
+                Console.WriteLine("OK Connecting " + input);
+                Console.ReadKey();
+                host = IPAddress.Parse(input);
+                int port = 8765;
+                IPEndPoint ipe = new IPEndPoint(host, port);
+                TcpListener svr = null;
+                string recvl = null;
+            }
+            else
+            {
+                Console.WriteLine("false");
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
